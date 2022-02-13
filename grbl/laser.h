d5cd683eea5eefb0db68144f37610aa9cd42b236
alnwlsn <alnwlsn@gmail.com>
2022-02-12 23:43:08 -0500

    added include guards

--- grbl/laser.h
+++ grbl/laser.h
@@ -1,7 +1,13 @@
 //alnwlsn's laser control - uses atmega Pin 45 with 9 bit (0-512) 4Khz PWM to control a laser
 //timer 5 is used
 
+#ifndef laser_h
+#define laser_h
+
 #include "grbl.h"
 
 void laser_init(); //initializes the servo code
 void laser_duty(uint16_t duty); 
+
+
+#endif
